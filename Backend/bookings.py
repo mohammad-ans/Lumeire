3a41c9044ca998692f50367731dab96497ef990f
mohammad-ans <ansmuhammad340@gmail.com>
2026-07-30 21:07:21 +0500

    Created endpoints for bookings creation, deletion, fetching and getting salons and their services

--- Backend/bookings.py
+++ Backend/bookings.py
@@ -1,3 +1,62 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from typing import List
+from database import get_db
+from models import Booking, Salon, Service, Stylist, User
+from schemas import BookingCreateRequest, BookingResponse, SalonResponse, ServiceResponse
+
+from auth import get_current_user
 
 router = APIRouter(prefix="/bookings")
+
+@router.get("/salons", response_model=List[SalonResponse])
+def list_salons(db: Session = Depends(get_db)):
+    return db.query(Salon).all()
+
+@router.get("/salons/{salon_id}/services", response_model=List[ServiceResponse])
+def list_services(salon_id: str, db: Session = Depends(get_db)):
+    salon = db.query(Salon).filter(Salon.id == salon_id).first()
+    if not salon:
+        raise HTTPException(status_code=404, detail="Salon Not Found")
+    return db.query(Service).filter(Service.salon_id == salon_id).all()
+
+
+@router.post("/bookings", response_model=BookingResponse, status_code=201)
+def create_booking(data: BookingCreateRequest, db: Session = Depends(get_db), current_user: Depends()get_current_user)):
+    service = db.query(Service).filter(Service.id == data.service_id, Service.salon_id == data.salon_id).first()
+    if not service:
+        raise HTTPException(status_code=404, detail="Service not found for this salon")
+    if data.stylist_id:
+        stylist = db.query(Stylist).filter(Stylist.id == data.stylist_id, Stylist.salon_id == data.salon_id).first()
+        if not stylist:
+            raise HTTPException(status_code=404, detail="Stylist not found for this salon")
+
+    booking = Booking(
+    user_id = current_user.id,
+    salon_id = data.salon_id,
+    stylist_id = data.stylist_id,
+    appointment_time = data.appointment_time,
+    status = "Upcoming",
+    total_amount = service.price
+    )
+    db.add(booking)
+    db.commit()
+    db.refresh(booking)
+    return booking
+
+@router.get("/bookings", response_model=List[BookingResponse])
+def list_bookings(db : Session = Depends(get_db), current_user: User = Depends(get_current_user)):
+    return db.query(Booking).filter(Booking.user_id == current_user.id).order_by(Booking.appointment_time.desc()).all()
+
+@router.delete("/bookings/{booking_id}", response_model=BookingResponse)
+def cancel_booking(booking_id: str, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
+    booking = db.query(Booking).filter(Booking.id == booking_id, Booking.user_id == current_user.id).first()
+    if not booking:
+        raise HTTPException(status_code=404, detail="Booking Not found")
+    if booking.status == "Cancelled":
+        raise HTTPException(status_code=400, detail="Booking is already cancelled")
+
+    booking.status = "Cancelled"
+    db.commit()
+    db.refresh(booking)
+    return booking
--- Backend/schemas.py
+++ Backend/schemas.py
@@ -1,5 +1,6 @@
 from typing import Optional
 from pydantic import BaseModel, EmailStr, ConfigDict
+from datetime import datetime
 
 class SignUp(BaseModel):
     email: EmailStr
@@ -53,3 +54,45 @@ class ProfileUpdateRequest(BaseModel):
     phone: Optional[str] = None
     date_of_birth: Optional[str] = None
     fcm_token: Optional[str] = None
+
+class SalonResponse(BaseModel):
+    id: str
+    name: str
+    category: str
+    address: str
+    latitude: str
+    longitude: str
+    rating: float = 0.0
+    review_count: int = 0
+    phone: Optional[str] = None
+    website: Optional[str] = None
+    openTime: Optional[str] = None
+    closeTime: Optional[str] = None
+
+    model_config = ConfigDict(from_attributes=True)
+
+class ServiceResponse(BaseModel):
+    id: str
+    name: str
+    category: Optional[str] = None
+    duration_minutes: int
+    price: float
+    salon_id: str
+
+class BookingCreateRequest(BaseModel):
+    salon_id: str
+    service_id: str
+    stylist_id: Optional[str] = None
+    appointment_time: datetime
+
+class BookingResponse(BaseModel):
+    id: str
+    user-id: str
+    salon_id: str
+    stylist_id: Optional[str] = None
+    appointment_time: datetime
+    status: str
+    total_amount: float
+    created_at: datetime
+
+    model_config = ConfigDict(from_attributes=True)
